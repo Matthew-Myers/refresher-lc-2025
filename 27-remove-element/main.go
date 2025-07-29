c8887862d68de64308033b3ba83b192caf0d78f3
matthew.myers <myersmatt191@gmail.com>
2025-07-29 11:31:05 -0400

    push

--- 27-remove-element/main.go
+++ 27-remove-element/main.go
@@ -2,6 +2,12 @@ package removeelement
 
 import "slices"
 
+/*
+Quick impl using the question's range of numbers of 1<=n<=50 to make sure that our numbers end up on the back.
+O(n log(n)) runtime complexity because of the slices.Sort() quicksort
+
+Could make this O(n) by replacing and swapping elements as we go with two pointers
+*/
 func removeElement(nums []int, val int) int {
 	placeholder := 55
 	replaceCount := 0
@@ -13,4 +19,19 @@ func removeElement(nums []int, val int) int {
 	}
 	slices.Sort(nums)
 	return len(nums) - replaceCount
+}
+
+/* O(n) imple w/ O(1) space
+Just overwrite the element when need be, don't need to worry about appending the element to the end
+*/
+
+func removeElement(nums []int, val int) int {
+	i := 0
+	for j := 0; j < len(nums); j++ {
+		if nums[j] != val {
+			nums[i] = nums[j]
+			i++
+		}
+	}
+	return i
 }
